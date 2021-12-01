fb7821d3d859cb021717313ee27b0fca623416ad
gmoneyomics <47872912+gmoneyomics@users.noreply.github.com>
2021-11-30 19:53:05 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,36 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# my functions create a special "matrix" object that can cache its inverse and then cachesolve should retrieve the inverse from the cache
 
+# first make a matrix tha can cache its inverse
+# make input x a matrix 
+# change all m to s for  solve, replace all mean function with solve function
+
+makeCacheMatrix <- function(x = matrix(sample(1:50,2),4,4)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setmean <- function(solve) s <<- solve
+  getmean <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# then retrieve the inverse from the cache if  it is  there
+# also change mean to solve and m to s
+cachemean <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(s)) {
+    message("get inverse matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
